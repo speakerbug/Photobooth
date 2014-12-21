b80802fab79f5ca54ba64f2372286402ce9f263c
Henry Saniuk <henry@henrysaniuk.com>
2014-12-20 22:09:22 -0500

    Take 9023453

--- buttontakepic2.py
+++ buttontakepic2.py
@@ -1,27 +1,12 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
-GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-
-	GPIO.wait_for_edge(23, GPIO.RISING)
-
-	print("Button 1 Pressed")
-
-	GPIO.wait_for_edge(23, GPIO.FALLING)
-
-	print("Button 1 Released")
-
-	GPIO.wait_for_edge(24, GPIO.FALLING)
-
-	print("Button 2 Pressed")
-
-	GPIO.wait_for_edge(24, GPIO.RISING)
-
-	print("Button 2 Released")
-
-GPIO.cleanup()
+    input_state = GPIO.input(18)
+    if input_state == False:
+        print('Button Pressed')
+        time.sleep(0.2)
