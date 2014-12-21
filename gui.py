dfc8110c254d32da0dd9abd311f381506ad656e9
Henry Saniuk <henry@henrysaniuk.com>
2014-12-20 19:50:02 -0500

    Made upload it's on method

--- gui.py
+++ gui.py
@@ -52,29 +52,31 @@ def takeAPic():
         camera.stop_preview()
         sleep(1)
         v.set("Picture taken...Uplading online...")
+        uploadPic()
         sleep(1)
-        #upload online
-        from poster.encode import multipart_encode
-        from poster.streaminghttp import register_openers
-        import urllib2
-        
-        # Register the streaming http handlers with urllib2
-        register_openers()
-        
-        # Start the multipart/form-data encoding of the file "DSC0001.jpg"
-        # "image1" is the name of the parameter, which is normally set
-        # via the "name" parameter of the HTML <input> tag.
-        
-        # headers contains the necessary Content-Type and Content-Length
-        # datagen is a generator object that yields the encoded parameters
-        datagen, headers = multipart_encode({"image": open("image.jpg", "rb")})
-        
-        # Create the Request object
-        request = urllib2.Request("http://henrysaniuk.com/upload/index.php", datagen, headers)
-        # Actually do the request, and get the response
-        print urllib2.urlopen(request).read()
         v.set("Ready for another photo")
  
+def uploadPic():
+    #upload online
+    from poster.encode import multipart_encode
+    from poster.streaminghttp import register_openers
+    import urllib2
+    # Register the streaming http handlers with urllib2
+    register_openers()
+    
+    # Start the multipart/form-data encoding of the file "DSC0001.jpg"
+    # "image1" is the name of the parameter, which is normally set
+    # via the "name" parameter of the HTML <input> tag.
+    
+    # headers contains the necessary Content-Type and Content-Length
+    # datagen is a generator object that yields the encoded parameters
+    datagen, headers = multipart_encode({"image": open("image.jpg", "rb")})
+    
+    # Create the Request object
+    request = urllib2.Request("http://henrysaniuk.com/upload/index.php", datagen, headers)
+    # Actually do the request, and get the response
+    print urllib2.urlopen(request).read()
+    
 def main():
     createDisplay()
  
