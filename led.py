792f03812629b75e9adda7ed281a6e1977dddae9
Henry Saniuk <henry@henrysaniuk.com>
2014-12-21 22:06:09 -0500

    Better LED code

--- led.py
+++ led.py
@@ -1,5 +1,17 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(7, GPIO.OUT)
-GPIO.output(7, GPIO.HIGH)
+import time
+# blinking function
+def blink(pin):
+        GPIO.output(pin,GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(pin,GPIO.LOW)
+        time.sleep(1)
+        return
+# to use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BOARD)
+# set up GPIO output channel
+GPIO.setup(11, GPIO.OUT)
+# blink GPIO17 50 times
+for i in range(0,50):
+        blink(11)
+GPIO.cleanup()
