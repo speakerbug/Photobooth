1d9c9097cb1e277a2cc745da7ab1a74fb81bf163
Henry Saniuk <henry@henrysaniuk.com>
2014-12-21 22:09:00 -0500

    BCM take 2

--- led.py
+++ led.py
@@ -10,8 +10,8 @@ def blink(pin):
 # to use Raspberry Pi board pin numbers
 GPIO.setmode(GPIO.BCM)
 # set up GPIO output channel
-GPIO.setup(11, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
 # blink GPIO17 50 times
 for i in range(0,50):
-        blink(11)
+        blink(17)
 GPIO.cleanup()
